f620b6ab50cb70cd6ae880e3df02541730560661
박현우 <phw@baghyeon-uui-MacBookPro-2.local>
2022-05-24 13:42:07 +0900

    프로젝트 생성

---
+++ NaverSeriesonMainClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaverSeriesonMainClone
+//
+//  Created by 박현우 on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
