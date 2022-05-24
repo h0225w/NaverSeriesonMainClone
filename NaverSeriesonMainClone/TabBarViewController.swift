f8e958800c235fca9e58a5cd72c5697ba7a7e795
박현우 <phw@baghyeon-uui-MacBookPro-2.local>
2022-05-24 15:28:37 +0900

    탭 바 구현 및 각 탭마다 ViewController 생성

--- NaverSeriesonMainClone/TabBarViewController.swift
+++ NaverSeriesonMainClone/TabBarViewController.swift
@@ -7,23 +7,16 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setTabBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setTabBar() {
+        self.tabBar.tintColor = .white
+        self.tabBar.unselectedItemTintColor = .systemGray3
     }
-    */
-
 }
